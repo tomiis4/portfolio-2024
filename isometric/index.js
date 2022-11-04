8effd79c1021112d613bf87a4b9674b718bcf68d
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2022-11-04 18:06:08 +0100

    Update index.js

--- isometric/index.js
+++ isometric/index.js
@@ -1,26 +1,22 @@
 // constant variables
 const container = document.getElementById('game-container');
+const score = document.getElementById('score');
 const playerWidth = 100;
 const blockWidth = 150;
 const boardArray = [
-	// [0,0, 'spawn-point'], [1,0], [2,0], [3,0], [4,0], [5,0],
-	// [0,1], [1,1], [2,1], [3,1], [4,1], [5,1],
-	// [0,2], [1,2], [2,2], [3,2], [4,2], [5,2],
-	// [0,3], [1,3], [2,3], [3,3], [4,3], [5,3],
-	// [0,4], [1,4], [2,4], [3,4], [4,4], [5,4],
-	// [0,5], [1,5], [2,5], [3,5], [4,5], [5,5, 'end-point'],
-
-	[0, 0, 'spawn-point'], [1, 0], [2, 0],
-	[2, 1],
-	[2, 2],
-	[2, 3], [3, 3],
-	[3, 4], [4, 4, 'end-point'],
+	[0,0, 'spawn-point'], [1,0], [2,0], [3,0], [4,0], [5,0],
+	[0,1], [1,1], [2,1], [3,1], [4,1], [5,1],
+	[0,2], [1,2], [2,2, 'reward-point'], [3,2], [4,2], [5,2],
+	[0,3], [1,3], [2,3], [3,3], [4,3], [5,3],
+	[0,4, 'reward-point'], [1,4], [2,4], [3,4], [4,4], [5,4],
+	[0,5], [1,5], [2,5], [3,5], [4,5], [5,5, 'end-point'],
 ];
 
 // mutable variables
 let playerPosition = [];
 let endPoint = [];
-
+let collectedPoints = 0;
+let collectedPointsArray = [];
 
 // uilts
 
@@ -41,17 +37,26 @@ const insertElement = (type, xPos, yPos, array) => {
 		src="./images/block.png" 
 		id="brick"
 	>`;
+	
 	const specialEl = `<img 
 		style="margin-left: ${xPos}px; top: ${yPos}px"
 		src="./images/special-block.png" 
 		id="special"
 	>`;
-	const playerEl = `<img
-		style="margin-left: ${xPos}px; top: ${yPos - (playerWidth / 2)}px"
-		src="./images/player.png" 
-		id="player"
+	const rewardEl = `<img 
+		style="margin-left: ${xPos}px; top: ${yPos}px"
+		src="./images/reward-block.png" 
+		id="reward"
 	>`;
+	
+	const playerEl = `
+		<img
+			style="margin-left: ${xPos}px; top: ${yPos - (playerWidth / 2)}px"
+			src="./images/player-v3.png" 
+			id="player"
+		>`;
 
+	// check what type of block is it and add it to the container
 	switch (type) {
 		case 'spawn':
 			container.innerHTML += specialEl;
@@ -62,6 +67,9 @@ const insertElement = (type, xPos, yPos, array) => {
 		case 'end':
 			container.innerHTML += specialEl;
 			endPoint = array;
+			break;
+		case 'reward':
+			container.innerHTML += rewardEl;
 			break;
 		case 'brick':
 			container.innerHTML += brickEl;
@@ -77,8 +85,11 @@ const generateBoard = () => {
 		const xPosition = getPosition(array, blockWidth)[0]; 
 		const yPosition = getPosition(array, blockWidth)[1] + blockWidth * 2.5;
 
+		// check what type of the block is it
+		//TODO add type to the block instead of the undefined 
 		const getBlockType = array[2] == 'spawn-point' ? 'spawn'
 			: array[2] == 'end-point' ? 'end'
+			: array[2] == 'reward-point' ? 'reward'
 			: array[2] == undefined ? 'brick'
 			: 'other';
 		
@@ -86,11 +97,53 @@ const generateBoard = () => {
 	})
 }
 
+const getPoint = (player) => {
+	let allPoints = 0;
+
+ 	// get all points
+	boardArray.forEach((value) => {
+		if (value[2] == 'reward-point') {
+			allPoints++;
+		}
+	})
+	
+	// get current point 
+	boardArray.forEach((arr) => {
+		// check score
+		if (
+			arr[2] == 'reward-point' && arr[0] == playerPosition[0] 
+			&& arr[1] == playerPosition[1] 
+			&& !collectedPointsArray.includes(arr)
+		) {
+			const playerVersion = parseInt(player.src.split('')[player.src.split('').length-5])-1;
+
+			collectedPoints++;
+			collectedPointsArray.push(arr);
+
+			score.innerText = `Score: ${collectedPoints}`;
+			player.src = `./images/player-v${playerVersion-1}.png`;
+		}
+		
+		// check win
+		if (
+			arr[2] == 'end-point' && arr[0] == playerPosition[0] 
+			&& arr[1] == playerPosition[1] 
+			&& allPoints == collectedPoints
+		) {
+			score.innerText = 'You won!';
+			console.log('You won!');
+		}
+	})
+
+}
+
 const movePlayer = (direction) => {
+	// duplicate array
 	const previousPosition = playerPosition.slice();
 	
 	let isValid = [];
 
+	// get direction and then count it using 2D array
 	switch (direction) {
 		case 'up':
 			playerPosition[0]--;
@@ -108,6 +161,7 @@ const movePlayer = (direction) => {
 			console.log(`Wrong direction: ${direction}`)
 	}
 	
+	// check if move was valid (if player move is in the array )
 	boardArray.forEach((array) => {
 		if (array[0] == playerPosition[0] && array[1] == playerPosition[1]) {
 			isValid.push(true);
@@ -117,10 +171,7 @@ const movePlayer = (direction) => {
 	})
 	
 	if (!isValid.includes(true)) {
-		console.log('Wrong')
 		playerPosition = previousPosition;
-	} else {
-		console.log('corre')
 	}
 	
 	const player = document.getElementById('player');
@@ -130,6 +181,8 @@ const movePlayer = (direction) => {
 
 	player.style.marginLeft = `${xPos}px`;
 	player.style.marginTop = `${yPos}px`;
+
+	getPoint(player);
 }
 
 const keyPress = (e) => {
