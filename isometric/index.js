3fe9958a96f064d5cbc9f9f7758c551db38bbd8d
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2022-11-05 22:26:56 +0100

    Update index.js

--- isometric/index.js
+++ isometric/index.js
@@ -1,11 +1,18 @@
-// constant variables
+//     VARIABLES     //
+
+
+// Constants
+
+// DOM elements
 const container = document.getElementById('game-container');
 const score = document.getElementById('score');
 
+// player informations
 const imagePath = './images/'
 const playerWidth = 100;
 const blockWidth = 150;
 
+// board
 const boardArray = [
 	[0,0, 'spawn-point'], [1,0], [2,0], [3,0], [4,0], [5,0],
 	[0,1], [1,1, 'spike'], [2,1], [3,1], [4,1], [5,1],
@@ -15,89 +22,118 @@ const boardArray = [
 	[0,5], [1,5], [2,5], [3,5], [4,5], [5,5, 'end-point'],
 ];
 
-// mutable variables
+
+// Mutable
+
+// player
 let playerPosition = [];
+
+// board
 let endPoint = [];
 let collectedPoints = 0;
 let collectedPointsArray = [];
+
+// obstacles
 let isSpikeOn;
 
-// uilts
 
-// delay
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+//     UILTS     //
 
-// get x,y position in pixels trough 2d matrix
-// arguments: array of 2D pixels and item width
+
+// Delay
+
+const delay = (ms) => {
+	return new Promise(res => setTimeout(res, ms));
+}
+
+// Get position
+
+// arguments 2d array and block width px
+// return x,y in pixels
 const getPosition = (array, width) => {
 	// calculate position and scale it to the block size
 	const getX = (array[0] * 1 + array[1] * -1) * (width / 2);
 	const getY = (array[0] * 0.5 + array[1] * 0.5) * (width / 2);
-
+	
 	return [getX, getY];
 }
 
-const setScore = (value, isCustom) => {
-	isCustom == 1 ? score.innerText = value : score.innerText = `Score: ${value}`;
+// Set score
+
+// arguments score, isCustom
+const setScore = (scoreVal, isCustom) => {
+	if (isCustom) {
+		score.innerText = scoreVal;
+	} else {
+		score.innerText = `Score: ${scoreVal}`;
+	}
 }
 
-// insert element to the container
-const insertElement = (type, xPos, yPos, array) => {
-	const brickEl = `<img 
-		style="margin-left: ${xPos}px; top: ${yPos}px"
-		src="${imagePath}block.png" 
-		id="brick"
-	>`;
-	
-	const specialEl = `<img 
-		style="margin-left: ${xPos}px; top: ${yPos}px"
-		src="${imagePath}special-block.png" 
-		id="special"
-	>`;
-	const rewardEl = `<img 
-		style="margin-left: ${xPos}px; top: ${yPos}px"
-		src="${imagePath}reward-block.png" 
-		id="reward"
-	>`;
+// Insert element
 
-	const spikeEl = `<img 
-		style="margin-left: ${xPos}px; top: ${yPos}px"
-		src="${imagePath}spike-0.png" 
-		id="spike"
-	>`;
-	
-	const playerEl = `
-		<img
+// arguments type of element, x,y position in px, 2d array
+const insertElement = ({type, xPos, yPos, array}) => {
+	const elements = {
+		brick: `<img 
+			style="margin-left: ${xPos}px; top: ${yPos}px"
+			src="${imagePath}block.png" 
+			id="brick"
+			>`,
+		special: `<img 
+			style="margin-left: ${xPos}px; top: ${yPos}px"
+			src="${imagePath}special-block.png" 
+			id="special"
+			>`,
+		reward: `<img 
+			style="margin-left: ${xPos}px; top: ${yPos}px"
+			src="${imagePath}reward-block.png" 
+			id="reward"
+			>`,
+		spike: `<img 
+			style="margin-left: ${xPos}px; top: ${yPos}px"
+			src="${imagePath}spike-0.png" 
+			id="spike"
+			>`,	
+		player: `<img
 			style="margin-left: ${xPos}px; top: ${yPos - (playerWidth / 2)}px"
 			src="${imagePath}player-v3.png" 
 			id="player"
-		>`;
+			>`,
+	};
 
-	// check what type of block is it and add it to the container
+	// check type
 	switch (type) {
+		// blocks
 		case 'spawn':
-			container.innerHTML += specialEl;
-			container.innerHTML += playerEl;
+			container.innerHTML += elements.special;
+			container.innerHTML += elements.player;
+			// playerPosotion = array;
 			playerPosition[0] = array[0];
 			playerPosition[1] = array[1];
-			break;
+			break;	
 		case 'end':
-			container.innerHTML += specialEl;
+			container.innerHTML += elements.special;
 			endPoint = array;
-			break;
+			break;	
 		case 'reward':
-			container.innerHTML += rewardEl;
+			container.innerHTML += elements.reward;
 			break;
 		case 'brick':
-			container.innerHTML += brickEl;
+			container.innerHTML += elements.brick;
 			break;
+		
+		// obstacles
 		case 'spike':
-			container.innerHTML += spikeEl;
+			container.innerHTML += elements.spike;
 			break;
+		
+		// default
 		default:
 			console.log(`Wrong type ${type}`);
 	}
 }
+
+// Check lose
 
 const checkLose = () => {
 	if (collectedPoints <= -1) {
@@ -105,96 +141,176 @@ const checkLose = () => {
 	}
 }
 
+// Check win
+
+const checkWin = () => {
+	let allPoints = 0;
+
+	boardArray.forEach((arr) => {
+		// get all points
+		if (arr[2] == 'reward-point') {
+			allPoints++;
+		}
+		
+		if (
+			arr[2] == 'end-point'
+			
+			&& arr[0] == playerPosition[0]
+			&& arr[1] == playerPosition[1]
+			
+			&& allPoints == collectedPoints
+		) {
+			setScore('You won', 1);
+		}
+	})
+}
+
+// Check obstacle
+
+const checkObstacle = () => {
+	boardArray.forEach((arr) => {
+		if (
+			arr[2] == 'spike' // add move in future
+			&& arr[0] == playerPosition[0] 
+			&& arr[1] == playerPosition[1]
+			&& isSpikeOn == true // 
+		) {
+			collectedPoints--;
+			setScore(collectedPoints);
+		}
+	})
+}
+
+// Reset game
+
+const resetGame = () => {
+	// reset variables
+	playerPosition = [];
+	endPoint = [];
+	collectedPoints = 0;
+	collectedPointsArray = [];
+	isSpikeOn = null;
+	
+	// reset elements
+	container.innerHTML = '';
+	score.innerText = 'Score: 0';
+	
+	//activate game
+	generateBoard();
+}
+
+
+//     OBSTACLES     //
+
 
 const obstacles = () => {
-	const spikeArray = document.querySelectorAll('#spike');
-	
+	// elements of all obstacles
+	const spikesArray = document.querySelectorAll('#spike');
+
 	const spikes = async () => {
-		for (;;) {
-			// move spike up/down
+		while (true) {
+			// switch for spikes
 			if (isSpikeOn == true) {
 				isSpikeOn = false;
-				
-				spikeArray.forEach((elem) => {
+
+				// set spikes off
+				spikesArray.forEach((elem) => {
 					elem.src = `${imagePath}spike-0.png`;
-				})
-			} else {
+				});
+			} 
+			else {
 				isSpikeOn = true;
-				
-				spikeArray.forEach((elem) => {
+
+				// set spikes on
+				spikesArray.forEach((elem) => {
 					elem.src = `${imagePath}spike-1.png`;
-				})
+				});
 			}
 			
+			checkObstacle();
+			checkLose();
+			
+			// how fast will spikes change
 			await delay(1300);
 		}
 	}
 
+	// activate obstacles
 	spikes();
-} 
+}
+
+
+//     GAME MECHANIC	    //
+
+
+// Generate board
 
 const generateBoard = () => {
 	boardArray.forEach((array) => {
-		const xPosition = getPosition(array, blockWidth)[0]; 
-		const yPosition = getPosition(array, blockWidth)[1] + blockWidth * 2.5;
-
-		// check what type of the block is it
-		//TODO add type to the block instead of the undefined 
-		const getBlockType = array[2] == 'spawn-point' ? 'spawn'
+		// get position and center 
+		const xPosition = getPosition(array, blockWidth)[0];
+		const yPosition = getPosition(array, blockWidth)[1] + blockWidth;
+		
+		// check block type
+		// FIXME instead undefined add 'block' to board
+		const blockType = array[2] == 'spawn-point' ? 'spawn'
 			: array[2] == 'end-point' ? 'end'
 			: array[2] == 'reward-point' ? 'reward'
 			
+			// obstacles
 			: array[2] == 'spike' ? 'spike'
 			
+			// brick
 			: array[2] == undefined ? 'brick'
-			: 'other';
+			
+			// wrong type
+			: 'other'
 		
-		insertElement(getBlockType, xPosition, yPosition, array);
+		insertElement({
+			type: blockType,
+			xPos: xPosition,
+			yPos: yPosition,
+			array: array
+		});
 	})
 	
+	// activate obstacles
 	obstacles();
 }
 
+// Get point
+
+// arguments player element
 const getPoint = (player) => {
-	let allPoints = 0;
-
- 	// get all points
-	boardArray.forEach((value) => {
-		if (value[2] == 'reward-point') {
-			allPoints++;
-		}
-	})
-	
-	// get current point 
 	boardArray.forEach((arr) => {
-		// check score
+		// get current point
 		if (
-			arr[2] == 'reward-point' && arr[0] == playerPosition[0] 
+			arr[2] == 'reward-point'
+			&& arr[0] == playerPosition[0] 
 			&& arr[1] == playerPosition[1] 
-			&& !collectedPointsArray.includes(arr)
+			&& !collectedPointsArray.includes(arr) // if it's not collected
 		) {
-			const playerVersion = parseInt(player.src.split('')[player.src.split('').length-5]);
-
+			// get player state
+			const playerState = parseInt(
+				player.src.split('')[player.src.split('').length - 5]
+			);
+			
+			// collect points and score
 			collectedPoints++;
 			collectedPointsArray.push(arr);
-
 			setScore(collectedPoints);
-			player.src = `./images/player-v${playerVersion-1}.png`;
+			
+			player.src = `${imagePath}player-v${playerState-1}.png`;
 		}
 		
-		// check win
-		if (
-			arr[2] == 'end-point' && arr[0] == playerPosition[0] 
-			&& arr[1] == playerPosition[1] 
-			&& allPoints == collectedPoints
-		) {
-			setScore('You won!', 1)
-			console.log('You won!');
-		}
-	})
-
+	});
+	
+	checkObstacle();
+	checkWin();
 	checkLose();
 }
+
+// Move player
 
 const movePlayer = (direction) => {
 	// duplicate array
@@ -222,7 +338,10 @@ const movePlayer = (direction) => {
 	
 	// check if move was valid (if player move is in the array )
 	boardArray.forEach((array) => {
-		if (array[0] == playerPosition[0] && array[1] == playerPosition[1]) {
+		if (
+			array[0] == playerPosition[0] 
+			&& array[1] == playerPosition[1]
+		) {
 			isValid.push(true);
 		} else {
 			isValid.push(false);
@@ -233,29 +352,23 @@ const movePlayer = (direction) => {
 		playerPosition = previousPosition;
 	}
 	
+	// move player
 	const player = document.getElementById('player');
 	const getPlayerPosition = getPosition(playerPosition, blockWidth);
 	const xPos = getPlayerPosition[0];
 	const yPos = getPlayerPosition[1];
-
+	
 	player.style.marginLeft = `${xPos}px`;
 	player.style.marginTop = `${yPos}px`;
 
 	getPoint(player);
-
-	// check if he stepped on the spike
-	boardArray.forEach((arr) => {
-		if (
-			arr[2] == 'spike' 
-			&& arr[0] == playerPosition[0] 
-			&& arr[1] == playerPosition[1]
-			&& isSpikeOn == true // i don't need that
-		) {
-			collectedPoints--;
-			setScore(collectedPoints);
-		}
-	})
 }
+
+
+//     CONTROLS     //
+
+
+// Keypress
 
 const keyPress = (e) => {
 	switch (e.key) {
@@ -275,4 +388,6 @@ const keyPress = (e) => {
 			return;
 	}
 }
+
+// activate
 document.body.addEventListener("keypress", keyPress);
