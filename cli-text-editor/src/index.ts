85323a5fe67792619f45b00c3af8af1d25002165
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2022-11-22 21:33:30 +0100

    :eyes: Save file, exit file, mouse ui, new keys

--- cli-text-editor/src/index.ts
+++ cli-text-editor/src/index.ts
@@ -9,19 +9,22 @@ const settings = settingArray;
 
 stdin.setRawMode(true);
 stdin.resume();
-stdin.setEncoding('utf8');
+stdin.setEncoding('utf-8');
 
 let isFileSaved: boolean = true;
 let buffer: string[][] = [['Hello, how are you'], ['this is new line'], ['New line delete me '], ['end of line']];
 let editMode: 'NORMAL' | 'INSERT' = 'NORMAL';
 let cursor = {
-	character: '▊',
-	selectedCharacter: '',
 	line: 0,
 	letter: 0,
 };
 
 //--UILTS--//
+
+// const delay = (ms: number) => {
+// 	return new Promise(res => res, ms);
+// }
+
 const newLine = () => {
 	isFileSaved = false;
 	buffer.push(['']);
@@ -33,11 +36,30 @@ const deleteLine = (lineIndex: number) => {
 	cursor.line = cursor.line-1 == -1 ? 0 : cursor.line-1;
 }
 
-
 const checkFile = () => {
 	if (file != undefined && fs.existsSync(file)) {
 		buffer = openFile(file);
 	}
+}
+
+const saveFile = async () => {
+	let storeBuffer: string[]= [];
+
+	buffer.forEach((arr) => {
+		let storeArr: string[] = arr[0].split('');
+		
+		storeArr.push('\n');
+		storeBuffer.push(storeArr.join(''));
+	})
+
+	await fs.writeFile(`./${file}`, storeBuffer.join(''), (err: any) => {
+		if (err) {
+			console.log(err);
+		}
+	});
+	isFileSaved = true;
+	
+	showBuffer();
 }
 
 //--FUNCTIONS--//
@@ -45,7 +67,7 @@ const checkFile = () => {
 const showAirline = () => {
 	const modeText = `\x1b[45m mode: ${editMode} \x1b[0m`;
 	const lineText = `\x1b[44m line: ${cursor.line}/${buffer.length} \x1b[0m`;
-	const fileText = `\x1b[41m file: ${file} \x1b[0m`;
+	const fileText = `\x1b[41m file: ${isFileSaved==true?'':'*'}${file} \x1b[0m`;
 	console.log(`${modeText}${lineText}${fileText}`);
 }
 
@@ -70,8 +92,14 @@ const showBuffer = () => {
 				return;
 			}
 			
-			bufferLineSplit[cursor.letter] = cursor.character;
-			cursor.selectedCharacter = bufferLineSplit[cursor.letter];
+			let cursorLetter: string[][] = [
+				['\x1b[47m\x1b[30m'], // set white background and black letter
+				[bufferLineSplit[cursor.letter] ? bufferLineSplit[cursor.letter] : ' '],
+				['\x1b[0m'],
+				['\x1b[36m'],
+			];
+			
+			bufferLineSplit[cursor.letter] = cursorLetter.join('');
 			
 			console.log('\x1b[36m%s\x1b[0m', `${numberLine} : ${bufferLineSplit.join('')}`);
 		} else {
@@ -81,47 +109,77 @@ const showBuffer = () => {
 }
 
 //TODO
-// * delete
 // * arrows
+// * numbers
+// * enter key
 const insertMode = (keyPressed: string) => {
+	console.log(keyPressed)
 	let bufferLineSplit = buffer[cursor.line][0].split('');
 	
-	bufferLineSplit.splice(cursor.letter, 0, keyPressed);
-	cursor.letter++;
+	// check if you can delete
+	if (keyPressed == '\u0008' && bufferLineSplit.length == 1) return;
+	
+	// delete letter
+	if (keyPressed == '\u0008') {
+		bufferLineSplit.splice(cursor.letter, 1);
+		cursor.letter--;
+	}
+	
+	// tab (horizontal & vertical)
+	else if (keyPressed == '\u0009' || keyPressed == '\u000B') {
+		const tabSize = settings.TAB_SIZE ? settings.TAB_SIZE : 4;
+		
+		for (let i=0; i <tabSize; i++) {
+			bufferLineSplit.splice(cursor.letter, 0, ' ');
+			cursor.letter++;
+		}
+	}
+	
+	// enter key
+	else if (keyPressed == null) {
+		bufferLineSplit = [''];
+		cursor.letter = 0;
+		cursor.line += 1;
+	}
+	
+	// write letter
+	else {
+		bufferLineSplit.splice(cursor.letter, 0, keyPressed);
+		cursor.letter++;
+	}
 	
 	buffer[cursor.line] = [bufferLineSplit.join('')];
 	editMode = 'INSERT';
 	isFileSaved = false;
 	
-	showBuffer();
+	// showBuffer();
 }
 
-// FIXME
-const exitEditor = () => {
-	if (isFileSaved == true) {
-		process.exit();
-	} else {
-		if (!fs.existsSync(`./${file}`)) {
-			fs.appendFile(`./${file}`, buffer.join(''), (err: any) => {
-				if (err) {
-					console.log(err);
-				}
-			});
+const exitEditor = async () => {
+	let storeBuffer: string[]= [];
+
+	buffer.forEach((arr) => {
+		let storeArr: string[] = arr[0].split('');
+		
+		storeArr.push('\n');
+		storeBuffer.push(storeArr.join(''));
+	})
+
+	await fs.writeFile(`./${file}`, storeBuffer.join(''), (err: any) => {
+		if (err) {
+			console.log(err);
 		} else {
-			fs.writeFile(`./${file}`, buffer.join(''), (err: any) => {
-				if (err) {
-					console.log(err);
-				}
-			});
+			process.exit();
 		}
-	}
+	});
 }
 
 const input = () => {
 	stdin.on('data', (data: string) => {
 		// exit mode
-		if (data == settings.EXIT_MODE || data == 'p') {
+		if (data == '\u001B') {
 			editMode = 'NORMAL';
+			showBuffer();
 		}
 		// normal mode
 		if (editMode == 'NORMAL') {
@@ -129,8 +187,7 @@ const input = () => {
 				// important
 				case settings.EXIT || 'q':
 					exitEditor();
-					process.exit();
-				
+					break;
 				// move
 				case settings.MOVE_UP || 'k':
 					cursor.line = cursor.line-1 == -1 ? 0 : cursor.line-1;
@@ -158,13 +215,15 @@ const input = () => {
 					deleteLine(cursor.line)
 					showBuffer();
 					break;
+				case settings.SAVE_FILE || 'w':
+					saveFile();
+					break;
 				default: 
 					return;
 			}
 		}
 		// insert mode
 		else if (editMode == 'INSERT') {
-			data == 'q' ? process.exit() : console.log(data);
 			insertMode(data)
 			showBuffer();
 		}
