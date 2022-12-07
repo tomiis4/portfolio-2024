02d9259e4edbbe57c8ac9382ae87531f5bdb6404
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2022-12-07 16:30:26 +0100

    :eyes: Fixed

--- AOC/2022/Day-6/src/answer-2.ts
+++ AOC/2022/Day-6/src/answer-2.ts
@@ -10,75 +10,39 @@ const dataArray = (): string[] => {
 	}
 }
 
-const moveValue = (row: number, fromColumn: number, toColumn: number, containers: string[][]): string[][] => {
-	let newContainer: string[][] = containers;
-	
-	let movedValue: string[] = [];
-	let newColumn: string[] = [];
-	
-	for (let i=0; i < row; i++) {
-		movedValue.push(containers[fromColumn-1][i]);
-	}
-	movedValue.reverse();
-	for (let k=0; k < movedValue.length; k++) {
-		newColumn = containers[toColumn-1].splice(0,0, movedValue[k]);
-		newColumn = containers[toColumn-1]
+const isDuplicate = (arr: any[]) => {
+	let isDuplicateArr = [];
+	for (let i=0; i < arr.length; i++) {
+		let value = arr[i];
+		
+		if (arr.indexOf(value) == i && arr.lastIndexOf(value) == i) {
+			isDuplicateArr.push(true);
+		} else {
+			isDuplicateArr.push(false);
+		}
 	}
 	
-	for (let j=0; j < movedValue.length; j++) {
-		newContainer[fromColumn-1].shift();
-	}
-	
-	newContainer[toColumn-1] = newColumn;	
-	
-	return newContainer;
+	return isDuplicateArr.includes(false) ? true : false;
 }
 
 const getValues = () => {
-	const data = dataArray();
+	const data = dataArray()[0].split('');
 	
-	let containers: string[][] = [];
-	let instructions: string[][] = [];
+	let beforeData: string[] = [];
+	let result: null | number = null;
 	
-	let result = '';
-	
-	// parse values
-	data.forEach((value) => {
-		// blocks
-		if (value.trim().startsWith('[')) {
-			const blockArray = value.replace(/[\[\]]/g, '').replace(/    /g, '-').replace(/ /g, '-').split('-')
-			blockArray.forEach((value, index) => {
-				if (value != '') {
-					if (containers[index]) {
-						containers[index].push(value);
-					} else {
-						containers[index] = [value];
-					}
+	data.forEach((letter, index) => {
+		if (beforeData.length == 14) {
+			beforeData.shift();
+			beforeData.push(letter)
+			
+			if (isDuplicate(beforeData) == false) {
+				if (result == null) {
+					result = index+1;
 				}
-			})
-		}
-		
-		// instructions number
-		else if (value.startsWith('move')) {
-			const numberArray = value.replace(/[a-z]/g, '').trim().replace(/\s+/g, ',').split(',');
-			instructions.push(numberArray);
-		}
-	});
-	
-	for (let i=0; i < instructions.length; i++) {
-		const arr = instructions[i];
-		
-		const move = parseInt(arr[0]);
-		const from = parseInt(arr[1]);
-		const to = parseInt(arr[2]);
-		
-		containers = moveValue(move, from, to, containers);
-	}
-	
-	
-	containers.forEach((arr) => {
-		if (arr[0]) {
-			result += arr[0];
+			}
+		} else {
+			beforeData.push(letter)
 		}
 	})
 	
