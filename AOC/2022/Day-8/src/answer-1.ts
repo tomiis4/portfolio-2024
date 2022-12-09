74558d7e8fa5cd9c8702212c0e14f6c36dc8cb50
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2022-12-09 22:03:19 +0100

    Update answer-1.ts

--- AOC/2022/Day-8/src/answer-1.ts
+++ AOC/2022/Day-8/src/answer-1.ts
@@ -2,7 +2,7 @@ import fs from 'node:fs';
 
 const dataArray = (): string[] => {
 	try {
-		const data = fs.readFileSync('./test.txt', 'utf8');
+		const data = fs.readFileSync('./list.txt', 'utf8');
 		return data.split(/\r?\n/);
 	} catch (err) {
 		console.log(err);
@@ -14,7 +14,7 @@ const getGrid = (array: any[]): any[][] => {
 	let arr = new Array(array.length).fill(new Array(array[0].length));
 	
 	array.forEach((line, index) => {
-		const lineArr = line.split('');
+		const lineArr = line.trim().split('');
 		
 		arr[index] = lineArr;
 	})
@@ -25,7 +25,7 @@ const getGrid = (array: any[]): any[][] => {
 const getBorders = (arr: any[][]) => {
 	let onBorder = 0;
 	
-	onBorder += ((arr.length-2) * 2) + ((arr[0].length-1) * 2)
+	onBorder += ((arr.length-1) * 2) + ((arr[0].length-2) * 2);
 	
 	return onBorder;
 }
@@ -34,20 +34,21 @@ const getBorders = (arr: any[][]) => {
 const getValues = () => {
 	const data = dataArray();
 	const grid = getGrid(data)
+	const borders = getBorders(grid)
+
+	let isWrited: number[][] = [];
+	let result = borders;
 	
-	let isWrited = [];
-	let result = getBorders(grid);
-	
+	console.log(borders)
 
 	for (let yGrid=0; yGrid < grid.length; yGrid++) {
 		for (let xGrid=0; xGrid < grid[yGrid].length; xGrid++) {
-			if (yGrid == 0 || yGrid == grid.length-2) {
-				isWrited.push([parseInt(xGrid), parseInt(yGrid)]);
-				console.log(isWrited)
+			if (yGrid == 0 || yGrid == grid.length-2 || xGrid == 0 || xGrid == grid[yGrid].length-1) {
+				isWrited.push([xGrid, yGrid]);
 			}
 		}
 	}
-	 
+	
 	for (let y=0; y < grid.length; y++) {
 		for (let x=0; x < grid[y].length; x++) {
 			let horizontal = [];
@@ -56,70 +57,35 @@ const getValues = () => {
 			}
 			
 			let left = grid[y].slice(0, x);
-			let right = grid[y].slice(x);
+			let right = grid[y].slice(x+1);
 			
-			let top = horizontal.slice(0, x);
-			let bottom = horizontal.slice(x);
-			top.pop();
-			bottom.shift();
-			left.pop();
-			right.shift();
-
+			let top = horizontal.slice(0, y);
+			let bottom = horizontal.slice(y+1);
+			
+			// bottom.shift();
+			// right.shift();
+			
 			// left
-			if (
-				isWrited.some((arr) => arr[0] == parseInt(x) && arr[1] == parseInt(y))
-				
-				&& (left.some((elem) => parseInt(elem) >= grid[y][x]) == false
-				|| right.some((elem) => parseInt(elem) >= grid[y][x]) == false
-				|| top.some((elem) => parseInt(elem) >= grid[y][x]) == false
-				|| bottom.some((elem) => parseInt(elem) >= grid[y][x]) == false)
-			) {
-				// console.clear()
-				// console.log(isWrited);
-				isWrited.push([x,y]);
-				result++;
+			if (!isWrited.join('|').includes(`${x},${y}`)) {
+				if ( !left.some((elem) => parseInt(elem) >= parseInt(grid[y][x])) ) {
+					// console.log(x,y, 'Left', left)
+					result++;
+				}
+				else if ( !right.some((elem) => parseInt(elem) >= parseInt(grid[y][x])) ) {
+					// console.log(x,y, 'Right', right)
+					result++;
+				}
+				else if ( !top.some((elem) => parseInt(elem) >= parseInt(grid[y][x])) ) {
+					// console.log(x,y, 'Top', top)
+					result++;
+				}
+				else if ( !bottom.some((elem) => parseInt(elem) >= parseInt(grid[y][x])) ) {
+					// console.log(x,y, 'Bottom', bottom)
+					result++;
+				}
 			}
 		}
 	}
-	
-	
-	// grid.forEach((arr: any[], index) => {
-	// 	if (index==0 || grid.length==index+1) return;
-		
-		
-		
-		
-	// 	arr.forEach((value, numI) => {
-	// 		// const number = parseInt(value);
-	// 		const checkLeft = (item: any) =>  parseInt(item) >= parseInt(value[numI]); 
-
-	// 		if (
-	// 			!arr.some(checkLeft)
-	// 		) {
-	// 			console.log('If');
-	// 			result++;
-	// 		}
-	// 		// if (
-	// 		// 	// left/right
-	// 		// 	(number < arr[numI-1])
-	// 		// 	&& (number < arr[numI+1])
-				
-	// 		// 	// top
-	// 		// 	&& (number < grid[index-1][numI-1])
-	// 		// 	&& (number < grid[index-1][numI])
-	// 		// 	&& (number < grid[index-1][numI+1])
-				
-	// 		// 	// top bottom
-	// 		// 	&& (number < grid[index+1][numI-1])
-	// 		// 	&& (number < grid[index+1][numI])
-	// 		// 	&& (number < grid[index+1][numI+1])
-	// 		// ) {
-	// 		// 	console.log('If');
-	// 		// 	result++;
-	// 		// }
-	// 	});
-	// });
-	
 	
 	return result;
 }
