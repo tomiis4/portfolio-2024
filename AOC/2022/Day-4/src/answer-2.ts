ff7eb387a8622f994bf6795152d6ae840010ca93
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2022-12-13 17:03:15 +0100

    Update answer-2.ts

--- AOC/2022/Day-4/src/answer-2.ts
+++ AOC/2022/Day-4/src/answer-2.ts
@@ -2,7 +2,7 @@ import fs from 'node:fs';
 
 const dataArray = (): string[] => {
 	try {
-		const data = fs.readFileSync('./test.txt', 'utf8');
+		const data = fs.readFileSync('./list.txt', 'utf8');
 		return data.split(/\r?\n/);
 	} catch (err) {
 		console.log(err);
@@ -10,5 +10,29 @@ const dataArray = (): string[] => {
 	}
 }
 
+const getValues = () => {
+	const data = dataArray();
+	let result = 0;
+	
+	data.forEach((value) => {
+		if (value=='') return;
+		
+		const str1_1= parseInt(value.split("-")[0]);
+		const str1_2= parseInt(value.split("-")[1].split(',')[0]); 
+		const str2_1= parseInt(value.split("-")[1].split(',')[1]);
+		const str2_2= parseInt(value.split("-")[2]);
+		
+		if (
+			(str1_1 < str2_1 && str1_2 < str2_1)
+			|| (str1_1 > str2_2 && str1_2 > str2_2)
+		) {
+			result;
+		} else {
+			result++;
+		}
+	})
+	
+	return result;
+}
 
-console.log(x());
+console.log(getValues());
