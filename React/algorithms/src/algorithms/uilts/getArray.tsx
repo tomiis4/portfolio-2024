d2a884325169b27ed866fddf31a4e024d29be29e
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2022-12-21 21:02:39 +0100

    new dir

---
+++ React/algorithms/src/algorithms/uilts/getArray.tsx
@@ -0,0 +1,9 @@
+const randNum (max: number) => Math.floor( Math.random() * max );
+
+function getArray(max: number, len: number): number[] {
+    let array: number[len] = [];
+
+
+}
+
+export default getArray;
