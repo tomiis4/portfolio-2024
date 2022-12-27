e6c8452da4a88f1a4225bc73124e1e2c83b70d41
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2022-12-27 19:31:41 +0100

    - removed old colision   - better child movment

--- Html/RPS/src/index.ts
+++ Html/RPS/src/index.ts
@@ -5,117 +5,211 @@ const width = window.innerWidth * 0.85, height = window.innerHeight * 0.85;
 canvasElem!.width = width;
 canvasElem!.height= height;
 
-let players = [
-	{
-		type: 'rock',
-		position: [0,0],
-		velocity: [0,0],
-		speed: 3,
-		
-		width: 50,
-		height: 50,
-		
-		child: [
-			{
-				position: [0,0],
-				width: 25,
-				height: 25
-			}
-		]
-	},
-	{
-		type: 'paper',
-		position: [200,200],
-		velocity: [0,0],
-		speed: 3,
-		
-		width: 50,
-		height: 50,
-	}
-];
+type Child = {
+	position: number[],
+	velocity: number[],
+	width: number,
+	height: number
+}
 
-let player = {
+type Player = {
+	type: 'rock' | 'paper' | 'scissors',
+	
+	position: number[],
+	velocity: number[],
+	
+	speed: number,
+	isMoving: boolean,
+	
+	width: number,
+	height: number,
+	
+	child: Child[]
+}
+
+let players: Player[] = [];
+let player: Player = {
 	type: 'rock',
 	position: [0,0],
 	velocity: [0,0],
 	speed: 3,
-	
 	width: 50,
-	height: 50,		
-	
+	height: 50,	
+	isMoving: false,
 	child: [
 		{
 			position: [0,75],
+			velocity: [0,0],
 			width: 25,
 			height: 25
 		},
 		{
-			position: [75,75],
+			position: [0,-75],
+			velocity: [0,0],
 			width: 25,
 			height: 25
 		}
 	]
 }
 
-const appendPlayer = (type: string, x:number,y:number, width:number, ctx:any) => {
-	ctx.fillStyle = type == 'rock' ? 'red' :
-		type == 'paper' ? 'green' :
-		type == 'scissors' ? 'blue' :
-		'white';
-	ctx.fillRect(x,y, width,width);
+// uilt
+// const isNegative = (n: number):boolean => n.toString().indexOf();
+const isNegative = (n: number):boolean => {
+	if (n.toString().indexOf('-') == -1) {
+		return false;
+	} else {
+		return true;
+	}
 }
 
+// append player on canvas
+const appendPlayer = () => {
+	players.forEach((obj) => {
+		const color = obj.type == 'rock' ? 'red' :
+			obj.type == 'paper' ? 'green' :
+			obj.type == 'scissors' ? 'blue' :
+			'white';
+		
+		// append parents
+		ctx!.fillStyle = color;
+		ctx!.fillRect(obj.position[0], obj.position[1], obj.width, obj.height);
+		
+		// append childs if any
+		if (obj.child[0]) {
+			obj.child.forEach((child) => {
+				ctx!.fillStyle = color;
+				ctx!.fillRect(child.position[0], child.position[1], child.width, child.height);
+			});
+		}
+	});
+}
+
+const changeChild = (key: string, position: 0|1, to:string | number) => {
+	player.child.forEach((obj) => {
+		obj[key][position] = to;
+	});
+}
+
+// Movment
 document.addEventListener('keydown', (e: KeyboardEvent) => {
+	let [vx, vy] = player.velocity;
+	const speed = player.speed;
+	
 	switch (e.code) {
 		case 'KeyW':
-			player.velocity[1] = -player.speed;
+			player.isMoving = true;
+			vy = -speed;
+			changeChild('velocity', 1, -speed);
 			break;
 		case 'KeyS':
-			player.velocity[1] = player.speed;
+			player.isMoving = true;
+			vy = speed;
+			changeChild('velocity', 1, speed);
 			break;
 		case 'KeyA':
-			player.velocity[0] = -player.speed;
+			player.isMoving = true;
+			vx = -speed;
+			changeChild('velocity', 0, -speed);
 			break;
 		case 'KeyD':
-			player.velocity[0] = player.speed;
+			player.isMoving = true;
+			vx = speed;
+			changeChild('velocity', 0, speed);
 			break;
-		default:
-			return;
+		default: return;
 	}
+	player.velocity = [vx,vy];
 })
-document.addEventListener('keyup', (e: KeyboardEvent) => {
-	if (e.code == 'KeyW' || e.code == 'KeyS') {
-		player.velocity[1] = 0;
-	} else if (e.code == 'KeyA' || e.code == 'KeyD') {
-		player.velocity[0] = 0
+document.addEventListener('keyup', () => player.isMoving = false );
+
+const resetVelocity = (velocity: number[], fadeOut: number): number[] => {
+	const isMoving = player.isMoving;
+	let [vx, vy] = velocity;
+	
+	// change
+	if (isNegative(vy) && isMoving == false && vy != 0) {
+		vy += fadeOut;
+	} else if (isMoving == false && vy != 0) {
+		vy -= fadeOut;
+	}	
+	if (isNegative(vx) && isMoving == false && vx != 0) {
+		vx += fadeOut;
+	} else if (isMoving == false && vx != 0) {
+		vx -= fadeOut;
 	}
-})
+	
+	// floating math fix
+	vy = vy<0.1&&vy>-0.1 ? 0 : vy;
+	vx = vx<0.1&&vx>-0.1 ? 0 : vx;
+	
+	return [vx,vy];
+}
+
+const checkDistance = (parent:number[], child:number[]):boolean => {
+	const maxDistance = 80;
+	const [pX, pY] = parent;
+	const [cX, cY] = child;
+	
+	if (Math.abs(pX-cX) <= maxDistance && Math.abs(pY-cY) <= maxDistance) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+const movePlayer = () => { 
+	
+	let [x,y] = player.position;
+	let [vx,vy] = player.velocity;
+	
+	// update player
+	x += vx;
+	y += vy;
+	
+	// update
+	player.position = [x,y];
+}
+
+const moveChild = () => {
+	if (player.child[0]) {
+		player.child.forEach((child) => {
+			let [x,y] = child.position;
+			let [vx,vy] = child.velocity;
+			
+			const rand = Math.floor(Math.random()*100) / 900; // smaller = less
+			const fadeOut = 0.2;
+			const parrentMiddle = [player.position[0]+player.width/2, player.position[1]+player.height/2];
+			const childMiddle = [child.position[0]+child.width/2, child.position[1]+height/2];
+			
+			// update player (how fast)
+			x += vx==0 ? 0
+				: checkDistance(parrentMiddle,childMiddle) ? vx
+				: isNegative(vx) ? vx-fadeOut
+				: vx+fadeOut;
+			y += vy==0 ? 0 
+				: checkDistance(parrentMiddle,childMiddle) ? vy
+				: isNegative(vy) ? vy-fadeOut
+				: vy+fadeOut;
+			
+			// update (how long)
+			child.position = [x,y];
+			child.velocity = resetVelocity(child.velocity, rand);
+		});
+	}
+}
 
 const updatePlayers = () => {
-	// move main object (X)
-	player.position[0] = player.position[0]+player.velocity[0] >= 0 && player.position[0]+player.velocity[0]+player.width <= width 
-		? player.position[0]+player.velocity[0] 
-		: player.position[0]
-	// move main object (Y)
-	player.position[1] = player.position[1]+player.velocity[1] >= 0 && player.position[1]+player.velocity[1]+player.width <= height
-		? player.position[1]+player.velocity[1] 
-		: player.position[1]
+	movePlayer();
+	moveChild();
 	
-	// move childs
-	player.child.forEach((child) => {
-		child.position[0] = child.position[0]+player.velocity[0] >= 0 && child.position[0]+player.velocity[0]+child.width <= width 
-			? child.position[0]+player.velocity[0] 
-			: child.position[0]
-		child.position[1] = child.position[1]+player.velocity[1] >= 0 && child.position[1]+player.velocity[1]+child.width <= width 
-			? child.position[1]+player.velocity[1] 
-			: child.position[1]
-	})
+	// reset velocity to 0
+	player.velocity = resetVelocity(player.velocity, 0.1);
 	
-	// append to all players
+	// append data to all players
 	players = [];
 	players.push(player);
 	
-	// testing
+	// testing opeonent (REMOVE)
 	players.push({
 		type: 'paper',
 		position: [200,200],
@@ -124,15 +218,18 @@ const updatePlayers = () => {
 		
 		width: 50,
 		height: 50,
+		isMoving: false,
 		
 		child: [
 		{
 			position: [-50,-50],
+			velocity: [0,0],
 			width: 25,
 			height: 25
 		},
 		{
 			position: [75,75],
+			velocity: [0,0],
 			width: 25,
 			height: 25
 		}
@@ -184,7 +281,7 @@ const checkColision = () => {
 			&& childBox[1] < enemyBox[1] + height
 			&& childHeight + childBox[1] > enemyBox[1]
 		) {
-			console.log('Child colision detected')
+			console.log('child-parent colision detected')
 		}
 		
 		// child & child
@@ -201,25 +298,18 @@ const checkColision = () => {
 				&& childBox[1] < oponentChBox[1] + childHeight
 				&& childHeight + childBox[1] > oponentChBox[1]
 			) {
-				console.log('Child&Child colision detected')
+				console.log('Child-Child colision detected')
 			}
 		})
 	});
 }
 
 const loop = (): void => {
-	ctx?.clearRect(0,0, width, height);
+	ctx!.clearRect(0,0, width, height);
 	
 	updatePlayers();
 	checkColision();
-	players.forEach((obj) => {
-		if (obj.child) {
-			obj.child.forEach((objCh) => {
-				appendPlayer(obj.type, objCh.position[0], objCh.position[1], objCh.width, ctx);
-			})
-		}
-		appendPlayer(obj.type, obj.position[0], obj.position[1], obj.width, ctx);
-	});
+	appendPlayer();
 	
 	window.requestAnimationFrame(loop);
 }
