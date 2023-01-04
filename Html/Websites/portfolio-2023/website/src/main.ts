784fd9731f3642fd0fa77b68b1d2abc92238ea86
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-01-04 19:09:05 +0100

    + new type

--- Html/Websites/portfolio-2023/website/src/main.ts
+++ Html/Websites/portfolio-2023/website/src/main.ts
@@ -2,24 +2,33 @@ import './style/global.scss';
 
 type Letter = {
 	letter: string,
-	position: number[]
+	position: number[],
+	type: 'welcome' | 'name'
 };
 
 const letters: Letter[] = [
-	{ letter: 'H', position: [50,84] },
-	{ letter: 'i', position: [254,-12] },
-	{ letter: ',', position: [108,68] },
-	{ letter: 'I', position: [6,29] },
-	{ letter: `'`, position: [50,24] },
-	{ letter: 'm', position: [57,105] }
+	{ letter: 'H', position: [50,84], type: 'welcome' },
+	{ letter: 'i', position: [254,-12], type: 'welcome' },
+	{ letter: ',', position: [108,68], type: 'welcome' },
+	{ letter: 'I', position: [6,29], type: 'welcome' },
+	{ letter: `'`, position: [50,24], type: 'welcome' },
+	{ letter: 'm', position: [57,105], type: 'welcome' },
+	
+	{ letter: 'T', position: [-219,105], type: 'name' },
+	{ letter: 'o', position: [-356,-19], type: 'name' },
+	{ letter: 'm', position: [-57,0], type: 'name' },
+	{ letter: 'i', position: [-306,54], type: 'name' },
+	{ letter: 'i', position: [-606,106], type: 'name' },
+	{ letter: 's', position: [-257,105], type: 'name' },
+	{ letter: '.', position: [-500,105], type: 'name' },
 ];
 const defaultLetters: Letter[] = [
-	{ letter: 'H', position: [0,0] },
-	{ letter: 'i', position: [-14,0] },
-	{ letter: ',', position: [-29,0] },
-	{ letter: 'I', position: [-16,0] },
-	{ letter: `'`, position: [-30,0] },
-	{ letter: 'm', position: [-46,0] }
+	{ letter: 'H', position: [0,0], type: 'welcome' },
+	{ letter: 'i', position: [-14,0], type: 'welcome' },
+	{ letter: ',', position: [-29,0], type: 'welcome' },
+	{ letter: 'I', position: [-16,0], type: 'welcome' },
+	{ letter: `'`, position: [-30,0], type: 'welcome' },
+	{ letter: 'm', position: [-46,0], type: 'welcome' }
 ];
 
 let isNormal = false;
@@ -31,8 +40,13 @@ const generate = async () => {
 	for (let i=0; i < letters.length; i++) {
 		const [x,y] = letters[i].position;
 		const letter = letters[i].letter;
+		const type = letters[i].type;
 		
-		container!.innerHTML += `<x id="letter-${letter== ',' ? 'comma' : letter== "'" ? 'quote' : letter}" style="left: ${x}px; top: ${y}px"> ${letter} </x>`;
+		if (type == 'welcome') {
+			container!.innerHTML += `<x id="letter-${letter== ',' ? 'comma' : letter== "'" ? 'quote' : letter}" style="left: ${x}px; top: ${y}px"> ${letter} </x>`;
+		} else {
+			container!.innerHTML += `<y id="letter-${letter== ',' ? 'comma' : letter== "'" ? 'quote' : letter}" style="left: ${x}px; top: ${y}px"> ${letter} </y>`;
+		}
 	}
 	isNormal = false;
 }
@@ -45,7 +59,6 @@ const getNormal = () => {
 		
 		const element = document.querySelector(`#letter-${letter== ',' ? 'comma' : letter== "'" ? 'quote' : letter}`);
 		
-		console.log(element,x,y, element.style.left)
 		
 		// @ts-ignore
 		element.style.left = `${x}px`;
