202c439b43da9059db374c1ac50d07bbeaf549e5
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-02-20 21:58:11 +0100

    + added slides

--- React/md-presentation/src/pages/create/components/Slides.tsx
+++ React/md-presentation/src/pages/create/components/Slides.tsx
@@ -1,11 +1,26 @@
-// import Slide from '../../../components/slide/Slide'
+import Slide from '../../../components/slide/Slide'
 import '../create.scss'
 
+
 // <Slide id={0} width={200} height={200} content={ headers: [{"X", [1,2],}] } >
+
+
+
 const Slides = () => {
+const content = {
+	headers: [
+		{
+			content: "Hello",
+			position: [0,0],
+		}
+	]
+}
 	return (
 		<div className='slides center'>
 			<div className='slides-wrapper center'>
+				<Slide id={0} height={100} ratio={49} content={content} />
+				<Slide id={0} height={100} ratio={49} content={content} />
+				<Slide id={0} height={100} ratio={49} content={content} />
 			</div>
 		</div>
 	)
