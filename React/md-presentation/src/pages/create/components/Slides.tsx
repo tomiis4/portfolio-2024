e77a105ea9f70557aae5987b582bbb51698b74bf
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-02-20 21:59:10 +0100

    + removed useless code

--- React/md-presentation/src/pages/create/components/Slides.tsx
+++ React/md-presentation/src/pages/create/components/Slides.tsx
@@ -2,10 +2,6 @@ import Slide from '../../../components/slide/Slide'
 import '../create.scss'
 
 
-// <Slide id={0} width={200} height={200} content={ headers: [{"X", [1,2],}] } >
-
-
-
 const Slides = () => {
 const content = {
 	headers: [
