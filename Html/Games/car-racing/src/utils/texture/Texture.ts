ff36ff2af6a3df4e5f2ce95752915328ec59cb68
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-03-05 21:45:43 +0100

    + new utility for texture

---
+++ Html/Games/car-racing/src/utils/texture/Texture.ts
@@ -0,0 +1,32 @@
+type TextureT = {
+	faces: number[][],
+	baseColor?: string,
+	color: 'red' | 'green' | 'blue' | 'null'
+}
+
+const Texture = (a: TextureT):string[] => {
+	let temp_texture:string[] = [];
+
+	if (a.baseColor) {
+		return new Array(a.faces.length).fill(a.baseColor);
+	}
+
+	while (temp_texture.length != a.faces.length) {
+		const colors = {
+			gray: 'rgb(100,100,100)', 
+			null: 'rgb(100,100,100)', 
+			blue: 'rgb(130,130,180)', 
+			green: 'rgb(130,180,130)', 
+			red: 'rgb(180,130,130)'
+		};
+
+		if (temp_texture.length % 2) {
+			temp_texture.push(colors.gray);
+		} else {
+			temp_texture.push(colors[a.color]);
+		}
+	}
+
+	return temp_texture;
+}
+export default Texture;
