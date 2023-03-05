0d055cff8f9f68d76d1f2d6dc2599f5362de92ed
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-03-05 21:43:56 +0100

    + working on floor

--- Html/Games/car-racing/src/main.ts
+++ Html/Games/car-racing/src/main.ts
@@ -1,6 +1,7 @@
 import './scss/style.scss'
 
 import Parser from './utils/parser/Parser';
+import Texture from './utils/texture/Texture';
 import Rotate from './utils/rotate/Rotate';
 
 const canvas = document.querySelector('canvas');
@@ -13,21 +14,135 @@ canvas!.height= height;
 type V3 = [number,number,number];
 type V2 = [number,number];
 
-type Car = {
-	rotation: V3,
-	position: V2,
+type Face = {
+	vertices: V3[],
+	face: number[],
 	scale: number,
-	color: 'red' | 'green' | 'blue'
+	color?: string
 }
 
-type Tree = {
+// type Car = {
+// 	rotation: V3,
+// 	position: V2,
+// 	scale: number,
+// 	color: 'red' | 'green' | 'blue'
+// }
+
+// type Tree = {
+// 	rotation: V3,
+// 	position: V3,
+// 	scale: number,
+// }
+
+type Floor = {
 	rotation: V3,
 	position: V2,
-	scale: number,
 }
 
-let players: Car[] = []; // [0] = local player
-let trees: Tree[] = [];
+type ModelData = {
+	vertices: V3[],
+	faces: number[][],
+	scale: number,
+	texture: string[]
+}
+
+// models
+const parsed_floor = await Parser('./src/objects/floor.obj');
+const floor_data: ModelData = {
+	vertices: parsed_floor.vertices,
+	faces: parsed_floor.faces,
+	scale: 1.5,
+	texture: Texture({
+		faces: parsed_floor.faces,
+		baseColor: '#1C1C1C',
+		color: 'null'
+	})
+}
+
+// let players: Car[] = []; // [0] = local player
+// let trees: Tree[] = [];
+let floor: Floor = {
+	rotation: [0,0,0],
+	position: [0,0],
+}
+
+
+// -- GLOBAL FUNCTIONS --
+const DrawModel = (e: {rotation: V3, data:ModelData}) => {
+	const [x,y,z] = e.rotation;
+
+	const rotate_x = Rotate('x', x, e.data.vertices);
+	const rotate_y = Rotate('y', y, rotate_x);
+	const rotate_z = Rotate('z', z, rotate_y);
+
+	for (let i=0; i < e.data.faces.length; i++) {
+		const face = e.data.faces[i];
+
+		DrawFace({
+			vertices: rotate_z,
+			face: face,
+			scale: e.data.scale,
+			color: e.data.texture[i]
+		})
+	}
+}
+
+const DrawFace = (e: Face) => {
+	ctx!.beginPath();   
+
+	// for each item in one face
+	for (let i=0; i < e.face.length; i++) {
+		ctx!.fillStyle = e.color ?? 'white';
+		const [x,y,_] = e.vertices[e.face[i]-1];
+
+		ctx!.lineTo(x * e.scale, y * e.scale)
+	}
+
+	ctx!.fill(); 
+}
+
+
+// -- APPEND FUNCTIONS --
+const append_floor = () => {
+	DrawModel({
+		rotation: floor.rotation,
+		data: floor_data,
+	})
+}
+
+// const append_car = () => {
+// 	players.push({
+// 		rotation: [0,0,0],
+// 		position: [0,0,0],
+// 		scale: 5,
+// 		color: 'green'
+// 	})
+// }
+
+// append_car();
+
+// const move_car = (side: 'x'|'z') => {
+// 	players[0]
+// }
+
+// controls
+// document.addEventListener('keydown', (e:KeyboardEvent) => {
+// 	switch (e.code) {
+// 		case 'KeyW':
+// 			// move Z +
+// 			break;
+// 		case 'KeyS':
+// 			// move Z -
+// 			break;
+// 		case 'KeyA':
+// 			// move X -
+// 			break;
+// 		case 'KeyD':
+// 			// move X +
+// 			break;
+// 		default: return
+// 	}
+// });
 
 // type V3 = [number,number,number]
 // type Face = {
@@ -100,9 +215,7 @@ let trees: Tree[] = [];
 const loop = () => {
 	ctx!.clearRect(0,0, width, height);
 
-	// draw_object([
-	// 	1,1,1
-	// ])
+	append_floor();
 
 	window.requestAnimationFrame(loop);
 }
