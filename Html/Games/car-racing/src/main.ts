734656c684882328a1535c98a77e8de3ed4c68be
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-03-10 15:59:36 +0100

    + add car

--- Html/Games/car-racing/src/main.ts
+++ Html/Games/car-racing/src/main.ts
@@ -1,8 +1,9 @@
 import './scss/style.scss'
 
 import Parser from './utils/parser/Parser';
-import Texture from './utils/texture/Texture';
 import Rotate from './utils/rotate/Rotate';
+import Texture from './utils/texture/Texture';
+import Gradient from './utils/gradient/Gradient';
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas!.getContext('2d');
@@ -19,15 +20,15 @@ type Face = {
 	vertices: V3[],
 	face: number[],
 	scale: number,
-	color?: string
+	color?: string,
+	transparent?: number,
+	stroke?: boolean
 }
 
-// type Car = {
-// 	rotation: V3,
-// 	position: V2,
-// 	scale: number,
-// 	color: 'red' | 'green' | 'blue'
-// }
+type Car = {
+	rotation: V3,
+	position: V2
+}
 
 // type Tree = {
 // 	rotation: V3,
@@ -44,34 +45,61 @@ type ModelData = {
 	vertices: V3[],
 	faces: number[][],
 	scale: number,
-	texture: string[]
+	texture: string[],
+	transparent?: number
+	stroke?: boolean
 }
 
-//data
-let camera = [0, 0, -5];
-let focalLength = 15;
+// global data
+const camera = [0, 0, -5];
+const focalLength = 15;
 
 // models
-const parsed_floor = await Parser('./src/objects/car.obj');
-let floor_data: ModelData = {
+const parsed_floor = await Parser('./src/objects/road.obj');
+const parsed_car = await Parser('./src/objects/car.obj');
+
+const floor_data: ModelData = {
 	vertices: parsed_floor.vertices,
 	faces: parsed_floor.faces,
-	scale: 150.0,
+	scale: 780.0,
+	transparent: 1,
 	texture: Texture({
 		faces: parsed_floor.faces,
-		// baseColor: '#FFFFFF',
+		baseColor: Gradient({
+			ctx: ctx!,
+			width: width,
+			height: height,
+			colors: ['rgb(20,20,20)', 'rgb(40,40,40)', 'rgb(230,230,230)', 'rgb(230,230,230)']
+
+		}),
 		// isRandom: true,
-		color: 'green'
+		color: 'null',
 	})
 }
 
-console.log(floor_data)
+let car_data: ModelData = {
+	vertices: parsed_car.vertices,
+	faces: parsed_car.faces,
+	scale: 55.0,
+	transparent: 0.7,
+	stroke: true,
+	texture: Texture({
+		faces: parsed_car.faces,
+		color: 'red',
+	})
+}
 
-// let players: Car[] = []; // [0] = local player
+
+let car: Car = {
+	rotation: [3.8,0,0],
+	position: [width/2, 615]
+};
+
 // let trees: Tree[] = [];
+
 let floor: Floor = {
-	rotation: [0,0,0],  // 0=front, 1=clock from top, 2=clock
-	position: [width/2, height/2]
+	rotation: [1.54,1.54,1.54],  // 0=front, 1=clock from top, 2=clock
+	position: [width/2, 400]
 }
 
 
@@ -100,7 +128,9 @@ const DrawModel = (e: {rotation: V3, data:ModelData, position: V2}) => {
 			vertices: rotate_z,
 			face: face,
 			scale: e.data.scale,
-			color: e.data.texture[i]
+			color: e.data.texture[i],
+			stroke: e.data.stroke,
+			transparent: e.data.transparent
 		})
 	}
 }
@@ -110,7 +140,7 @@ const DrawFace = (e: Face) => {
 
 	ctx!.lineWidth = 2;
 	ctx!.strokeStyle = "#CCCCCC";
-	ctx!.globalAlpha = 0.6;
+	ctx!.globalAlpha = e.transparent ?? 1;
 
 	// for each vertex in one face
 	for (let i=0; i < e.face.length; i++) {
@@ -118,7 +148,6 @@ const DrawFace = (e: Face) => {
 
 		const vertex = e.vertices[e.face[i]-1];
 		const [x,y] = ProjectVeretex(vertex);
-
 		ctx!.lineTo(x * e.scale + e.position[0], y * e.scale + e.position[1]);
 		
 		// to the first element to complete face
@@ -127,113 +156,90 @@ const DrawFace = (e: Face) => {
 
 			const vertex = e.vertices[e.face[0]-1];
 			const [x,y] = ProjectVeretex(vertex);
-
 			ctx!.lineTo(x * e.scale + e.position[0], y * e.scale + e.position[1]);
 		}
 	}
 
 	ctx!.fill();
-	ctx!.stroke();
+	e.stroke ? ctx!.stroke() : e.stroke
 }
 
 
-// -- APPEND FUNCTIONS --
-const append_floor = () => {
+// -- PROJECT FUNCTIONS --
+const project_floor = () => {
 	DrawModel({
 		rotation: floor.rotation,
 		position: floor.position,
-		data: floor_data,
+		data: floor_data
 	})
 }
 
-// const append_car = () => {
-// 	players.push({
-// 		rotation: [0,0,0],
-// 		position: [0,0,0],
-// 		scale: 5,
-// 		color: 'green'
-// 	})
-// }
+const project_car = () => {
+	DrawModel({
+		rotation: car.rotation,
+		position: car.position,
+		data: car_data
+	})
+}
 
-// append_car();
-
-// const move_car = (side: 'x'|'z') => {
-// 	players[0]
-// }
 
 // controls
 document.addEventListener('keydown', (e:KeyboardEvent) => {
 	switch (e.code) {
-		// case 'KeyQ':
-		// 	focalLength += 3
-		// 	break;
-		// case 'KeyE':
-		// 	focalLength -= 3
-		// 	break;
-		// case 'KeyW':
-		// 	camera[0] += 1;
-		// 	break;
-		// case 'KeyS':
-		// 	camera[0] -= 1;
-		// 	break;
-		// case 'KeyA':
-		// 	camera[1] += 1;
-		// 	break;
-		// case 'KeyD':
-		// 	camera[1] -= 1;
-		// case 'KeyX':
-		// 	camera[2] += 1;
-		// 	break;
-		// case 'KeyC':
-		// 	camera[2] -= 1;
-		// 	break;
 		case 'KeyW':
-			floor.rotation[0] += 0.02
+			car.rotation[0] += 0.02
 			break;
 		case 'KeyS':
-			floor.rotation[0] -= 0.02
+			car.rotation[0] -= 0.02
 			break;
 		case 'KeyA':
-			floor.rotation[1] += 0.02
+			car.rotation[1] += 0.02
 			break;
 		case 'KeyD':
-			floor.rotation[1] -= 0.02
+			car.rotation[1] -= 0.02
 			break;
 		case 'KeyU':
-			floor.rotation[2] += 0.02
+			car.rotation[2] += 0.02
 			break;
 		case 'KeyI':
-			floor.rotation[2] -= 0.02
+			car.rotation[2] -= 0.02
 			break;
 		case 'KeyQ':
-			floor.position[1] -= 5
+			car.position[1] -= 5
 			break;
 		case 'KeyE':
-			floor.position[1] += 5
-			break;
-		case 'KeyX':
-			floor.position[0] -= 5
-			break;
-		case 'KeyC':
-			floor.position[0] += 5
+			car.position[1] += 5
 			break;
 		case 'KeyF':
-			floor_data.scale -= 5
+			car_data.scale -= 5
 			break;
 		case 'KeyG':
-			floor_data.scale += 5
+			car_data.scale += 5
 			break;
 		default: return
 	}
-	console.log(floor)
-	console.log(floor_data.scale)
+	console.log(car)
+	console.log(car_data.scale)
 });
 
+const xI = document.querySelector('#x')
+const yI = document.querySelector('#y')
+const zI = document.querySelector('#z')
 
 const loop = () => {
 	ctx!.clearRect(0,0, width, height);
 
-	append_floor();
+	// controls (delete)
+	car.rotation[0] = parseInt(xI.value) /10
+	car.rotation[1] = parseInt(yI.value) /10
+	car.rotation[2] = parseInt(zI.value) /10
+
+	// car.rotation[0] = 1.52;
+	// car.rotation[1] = 152/100
+	// car.rotation[2] = 152/100
+
+	project_floor();
+	project_car();
 
 	window.requestAnimationFrame(loop);
 }
