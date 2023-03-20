2adbf183e4b59cb8db818dab2af3879ec351a136
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-03-20 21:53:09 +0100

    + navbar content

--- React/blog/src/pages/components/navbar/Navbar.tsx
+++ React/blog/src/pages/components/navbar/Navbar.tsx
@@ -1,1 +1,59 @@
+import './navbar.scss';
+import { useState } from 'react';
+import { FaGithub, FaInstagram } from 'react-icons/fa'
+import Link from './link/Link';
 
+function Navbar() {
+	const [isActive, setIsActive] = useState([true,false,false]);
+
+	const handleClick = (id: number) => {
+		let temp_active = new Array(3).fill(false);
+		temp_active[id] = true;
+
+		setIsActive(temp_active)
+	}
+
+	return (
+		<div className='navbar'>
+			<div className='socials'>
+				<div className='language'>
+					EN
+				</div>
+					<div className='separate'></div>
+				<a href='https://github.com/tomiis4'>
+					<FaGithub />
+				</a>
+					<div className='separate'></div>
+				<a href='https://instagram.com/tomii6_'>
+					<FaInstagram />
+				</a>
+			</div>
+
+			<div className='links'>
+				<Link 
+					id={0}
+					to={'#about-me'} 
+					name={'Home'} 
+					isActive={isActive[0]}
+					function={handleClick} 
+				/>
+				<Link 
+					id={1}
+					to={'#'} 
+					name={'Projects'} 
+					isActive={isActive[1]}
+					function={handleClick} 
+				/>
+				<Link 
+					id={2}
+					to={'#'} 
+					name={'Articles'} 
+					isActive={isActive[2]}
+					function={handleClick} 
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default Navbar;
