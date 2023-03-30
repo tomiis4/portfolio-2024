56d38d2f28429610f6e29d5acef60a0d77a8c1aa
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-03-30 23:12:57 +0200

    + routing for blog

--- React/blog/src/App.tsx
+++ React/blog/src/App.tsx
@@ -1,12 +1,15 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 import Info from "./pages/info/Info";
+import Blog from "./pages/blog/Blogs"
 
 function App() {
 	return (
 		<Router>
 			<Routes>
+				<Route path="*" element={<Info/>} />
 				<Route path="/" element={<Info/>} />
+				<Route path="/blog" element={<Blog/>} />
 			</Routes>
 		</Router>
 	);
