0e164a3b87a2bfe29bf5d3dbfd970fe13f965aa1
tomiis4 <tomas.kudynek@gmail.com>
2024-07-29 21:11:41 +0200

    update: full 360 rotating

--- Html/Games/party-dance/src/main.ts
+++ Html/Games/party-dance/src/main.ts
@@ -13,35 +13,42 @@ const ctx = canvas!.getContext('2d');
 // - přidávání objektů na mapu
 
 const width = 55;
-const img = document.querySelector("#i");
-const img2 = document.querySelector("#i2");
+const img = <CanvasImageSource>document.querySelector("#i")!;
+const img2 = <CanvasImageSource>document.querySelector("#i2")!;
 
-const drawRow = (row, nRow, image, data) => {
+const drawRow = (row: number[], nRow: number, image: CanvasImageSource, data: Object) => {
     const { type, rotation } = data
 
     row.forEach((block, nBlock) => {
-        let getX, getY = 0;
+        let getX = 0, getY = 0;
 
         if (block == 0) {
             return
         } else if (type == "up") {
-            getX = (nRow - nBlock) * (width / 2) - (nRow * 0.5 * width);
-            getY = (nRow + nBlock) / 2 * (width / 2) - (nRow * 0.75 * width);
+            getX = (nRow - nBlock) * (width / 2) - (nRow * 0.5 * width) - (width / 2);
+            getY = (nRow + nBlock) / 2 * (width / 2) - (nRow * 0.75 * width) - (width / 4);
         } else if (type == "down") {
             getX = (nRow - nBlock) * (width / 2);
             getY = (nRow + nBlock) / 2 * (width / 2);
         }
 
+        //FIXME UP only
         if (rotation == 90) {
             getX += (nBlock * width) - (width);
             getY += width / 2;
+        } else if (rotation == 180) {
+            getX -= width;
+            getY += width / 2;
+        } else if (rotation == 270) {
+            getX += (nBlock * width) - (width*2) ;
         }
+
 
         ctx!.drawImage(image, getX + 200, getY + 250, width, width);
     })
 }
 
-const drawObject = (object, image) => {
+const drawObject = (object: Object, image: CanvasImageSource) => {
     const data = object.data;
 
     if (object.type == "up") {
@@ -49,11 +56,22 @@ const drawObject = (object, image) => {
     }
 
     data.forEach((row, idx) => {
+        if (object.rotation != 90 && object.rotation != 180) {
+            row.reverse()
+        }
         drawRow(row, idx, image, object)
     })
 }
 
-const objects = {
+type Objects = { [key: string]: Object }
+type Object = {
+    type: "up" | "down",
+    rotation: number,
+    data: number[][]
+}
+
+
+const objects: Objects = {
     board: {
         type: "down",
         rotation: 0,
@@ -73,11 +91,11 @@ const objects = {
     },
     portal: {
         type: "up",
-        rotation: 90,
+        rotation: 270,
         data: [
-            [1, 1, 1],
             [1, 0, 1],
-            [1, 0, 0],
+            [1, 0, 1],
+            [1, 1, 0],
             [1, 0, 1],
             [1, 1, 1],
         ]
