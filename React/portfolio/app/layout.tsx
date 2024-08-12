ddf6c8bcec012e1fb8b2444cef093e54cc70cdd5
tomiis4 <tomas.kudynek@gmail.com>
2024-08-12 16:42:01 +0200

    add: new project

---
+++ React/portfolio/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.scss";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Tomáš Kudýnek | Home",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
