2afa10dd9b7c0ca2e6af22cf7de197e4cc2200c9
tomiis4 <tomas.kudynek@gmail.com>
2024-08-16 22:26:47 +0200

    add: projects scroll

--- React/portfolio/app/components/Page/Projects/Projects.tsx
+++ React/portfolio/app/components/Page/Projects/Projects.tsx
@@ -12,7 +12,7 @@ export default function Projects() {
         link: ["https://github.com", "GitHub"],
         text: "A powerful [NeoVim] plugin created to [increase] our regular expression ([RegExp]) writing and testing [experience]. Whether you're a [newbie] or [professional] developer, Hypersonic is here to make your life easier and [boost] your [productivity].",
         image: "/projects/hypersonic.png"
-    },{
+    }, {
         title: "Hypersonic.nvim",
         language: "Lua",
         year: "2023",
@@ -24,37 +24,38 @@ export default function Projects() {
     return (
         <section id="projects" className={style.projects}>
             <div className={style.box}>
-                {
-                    projects.map((project) => {
-                        return (
-                            <div className={style.card}>
-                                <Image
+                <div className={style.wrapper}>
+                    {
+                        projects.map((project) => {
+                            return (
+                                <div className={style.card}> <Image
                                     src={project.image}
                                     alt={"image"}
                                     fill
                                 />
-                                <h1>{project.title}</h1>
-                                <h4>
-                                    {project.language}
-                                    <span className={style.hl}> | </span>
-                                    {project.year}
-                                    <span className={style.hl}> | </span>
-                                    <Link href={project.link[0]}> {project.link[1]} </Link>
-                                </h4>
-                                <p>
-                                    {
-                                        project.text.split(' ').map((word, i) => {
-                                            return (
-                                                <span key={i} className={word.match(/\[.+\]/) ? style.hl : ""}>
-                                                    {word.replace(/\[(.+)\]/, '$1') + ' '}
-                                                </span>
-                                            );
-                                        })
-                                    }
-                                </p>
-                            </div>)
-                    })
-                }
+                                    <h1>{project.title}</h1>
+                                    <h4>
+                                        {project.language}
+                                        <span className={style.hl}> | </span>
+                                        {project.year}
+                                        <span className={style.hl}> | </span>
+                                        <Link href={project.link[0]}> {project.link[1]} </Link>
+                                    </h4>
+                                    <p>
+                                        {
+                                            project.text.split(' ').map((word, i) => {
+                                                return (
+                                                    <span key={i} className={word.match(/\[.+\]/) ? style.hl : ""}>
+                                                        {word.replace(/\[(.+)\]/, '$1') + ' '}
+                                                    </span>
+                                                );
+                                            })
+                                        }
+                                    </p>
+                                </div>)
+                        })
+                    }
+                </div>
                 <div className={style.switch}>
                     <button type="button"> &lt; </button>
                     {
