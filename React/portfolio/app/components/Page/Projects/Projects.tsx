5cb3c98c0bc4be0a1a27ed87e205a9726e8d67fb
tomiis4 <tomas.kudynek@gmail.com>
2024-08-16 20:11:22 +0200

    add: projects

--- React/portfolio/app/components/Page/Projects/Projects.tsx
+++ React/portfolio/app/components/Page/Projects/Projects.tsx
@@ -1,59 +1,71 @@
 import Image from "next/image";
 import style from "./projects.module.scss";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Projects() {
+    const [activeID, setActiveID] = useState(0);
     const projects = [{
         title: "Hypersonic.nvim",
         language: "Lua",
         year: "2023",
         link: ["https://github.com", "GitHub"],
         text: "A powerful [NeoVim] plugin created to [increase] our regular expression ([RegExp]) writing and testing [experience]. Whether you're a [newbie] or [professional] developer, Hypersonic is here to make your life easier and [boost] your [productivity].",
-        image: "https://github-production-user-asset-6210df.s3.amazonaws.com/87276646/356874661-7b99cc86-ca37-40ff-af6b-e1e80e1622d3.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240815%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240815T205224Z&X-Amz-Expires=300&X-Amz-Signature=d7900e7736125a6efb581b731582ac0cc142ad072ae82f1e5b8ea0bfa05071e6&X-Amz-SignedHeaders=host&actor_id=87276646&key_id=0&repo_id=593284732"
+        image: "/projects/hypersonic.png"
+    },{
+        title: "Hypersonic.nvim",
+        language: "Lua",
+        year: "2023",
+        link: ["https://github.com", "GitHub"],
+        text: "A powerful [NeoVim] plugin created to [increase] our regular expression ([RegExp]) writing and testing [experience]. Whether you're a [newbie] or [professional] developer, Hypersonic is here to make your life easier and [boost] your [productivity].",
+        image: "/projects/hypersonic.png"
     }]
-    // <Image src={"https://github.com/user-attachments/assets/7b99cc86-ca37-40ff-af6b-e1e80e1622d3"} width={500} height={200}  alt={"img"} />
-    //
-    // <h2>Hypersonic.nvim</h2>
-    // <h4>
-    //     Lua <span> | </span> 2023 <span> | </span> <Link href="#"> GitHub </Link>
-    // </h4>
-    // <p>
-    //
-    // </p>
+
     return (
         <section id="projects" className={style.projects}>
-            <div>
+            <div className={style.box}>
                 {
                     projects.map((project) => {
-                        return (<>
-                            <h1>{project.title}</h1>
-                            <h4>
-                                {project.language}
-                                <span className={style.hl}> | </span>
-                                {project.year}
-                                <span className={style.hl}> | </span>
-                                <Link href={project.link[0]}> {project.link[1]} </Link>
-                            </h4>
-                            <p>
-                                {
-                                    project.text.split(' ').map((word, i) => {
-                                        return (
-                                            <span key={i} className={word.match(/\[.+\]/) ? style.hl : ""}>
-                                                {word.replace(/\[(.+)\]/, '$1') + ' '}
-                                            </span>
-                                        );
-                                    })
-                                }
-                            </p>
-                            <Image
-                                src={project.image}
-                                alt={"image"}
-                                fill
-
-                            />
-                        </>)
+                        return (
+                            <div className={style.card}>
+                                <Image
+                                    src={project.image}
+                                    alt={"image"}
+                                    fill
+                                />
+                                <h1>{project.title}</h1>
+                                <h4>
+                                    {project.language}
+                                    <span className={style.hl}> | </span>
+                                    {project.year}
+                                    <span className={style.hl}> | </span>
+                                    <Link href={project.link[0]}> {project.link[1]} </Link>
+                                </h4>
+                                <p>
+                                    {
+                                        project.text.split(' ').map((word, i) => {
+                                            return (
+                                                <span key={i} className={word.match(/\[.+\]/) ? style.hl : ""}>
+                                                    {word.replace(/\[(.+)\]/, '$1') + ' '}
+                                                </span>
+                                            );
+                                        })
+                                    }
+                                </p>
+                            </div>)
                     })
                 }
+                <div className={style.switch}>
+                    <button type="button"> &lt; </button>
+                    {
+                        projects.map((_, i) => {
+                            return (
+                                <span key={i} className={i == activeID ? style.hl_bg : ''} />
+                            )
+                        })
+                    }
+                    <button type="button"> &gt; </button>
+                </div>
             </div>
         </section>
     )
