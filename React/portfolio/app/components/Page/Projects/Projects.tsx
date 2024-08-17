dfe866be786e41cac200bfa03bdc97162726ec1c
tomiis4 <tomas.kudynek@gmail.com>
2024-08-17 20:43:37 +0200

    add: projects scrolling

--- React/portfolio/app/components/Page/Projects/Projects.tsx
+++ React/portfolio/app/components/Page/Projects/Projects.tsx
@@ -1,11 +1,22 @@
 import Image from "next/image";
 import style from "./projects.module.scss";
 import Link from "next/link";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import useOnDisplay from "@/utils/useOnDisplay";
+
+type Project = {
+    title: string
+    language: string
+    year: `${number}`
+    link: string[]
+    text: string
+    image: string
+}
 
 export default function Projects() {
-    const [activeID, setActiveID] = useState(0);
-    const projects = [{
+    const [activeID, setActiveID] = useState<number>(0);
+    const wrapperRef = useRef<HTMLDivElement>(null);
+    const projects: Project[] = [{
         title: "Hypersonic.nvim",
         language: "Lua",
         year: "2023",
@@ -13,26 +24,67 @@ export default function Projects() {
         text: "A powerful [NeoVim] plugin created to [increase] our regular expression ([RegExp]) writing and testing [experience]. Whether you're a [newbie] or [professional] developer, Hypersonic is here to make your life easier and [boost] your [productivity].",
         image: "/projects/hypersonic.png"
     }, {
-        title: "Hypersonic.nvim",
+        title: "X",
         language: "Lua",
         year: "2023",
         link: ["https://github.com", "GitHub"],
         text: "A powerful [NeoVim] plugin created to [increase] our regular expression ([RegExp]) writing and testing [experience]. Whether you're a [newbie] or [professional] developer, Hypersonic is here to make your life easier and [boost] your [productivity].",
         image: "/projects/hypersonic.png"
-    }]
+    }, {
+        title: "Y",
+        language: "Lua",
+        year: "2023",
+        link: ["https://github.com", "GitHub"],
+        text: "A powerful [NeoVim] plugin created to [increase] our regular expression ([RegExp]) writing and testing [experience]. Whether you're a [newbie] or [professional] developer, Hypersonic is here to make your life easier and [boost] your [productivity].",
+        image: "/projects/hypersonic.png"
+    }];
+
+    const projectsRef = projects.map(() => useRef<HTMLDivElement>(null));
+    const visibleProjects = projectsRef.map((ref) => useOnDisplay(ref))
+
+
+    const handleScroll = () => {
+        let value = activeID;
+        visibleProjects.forEach((project, idx) => {
+            if (project == true && value != idx) {
+                value = idx
+            }
+        })
+        setActiveID(value)
+    };
+
+    const moveSide = (side: "left" | "right") => {
+        let value = activeID;
+        if (side == "left") {
+            value = activeID - 1 == -1 ? projects.length - 1 : activeID - 1
+        } else if (side == "right") {
+            value = (activeID + 1) % projects.length
+        }
+
+        setActiveID(value)
+        scrollTo(value)
+    }
+
+    const scrollTo = (id: number) => {
+        const width = wrapperRef!.current!.scrollLeftMax / (projects.length - 1)
+
+        setActiveID(id)
+        wrapperRef!.current!.scrollLeft = id * width
+    }
 
     return (
         <section id="projects" className={style.projects}>
             <div className={style.box}>
-                <div className={style.wrapper}>
+                <div className={style.wrapper} onWheel={handleScroll} onTouchMove={handleScroll} ref={wrapperRef}>
                     {
-                        projects.map((project) => {
+                        projects.map((project, idx) => {
                             return (
-                                <div className={style.card}> <Image
-                                    src={project.image}
-                                    alt={"image"}
-                                    fill
-                                />
+                                <div className={style.card} ref={projectsRef[idx]}>
+                                    <Image
+                                        src={project.image}
+                                        alt={"image"}
+                                        fill
+                                    />
                                     <h1>{project.title}</h1>
                                     <h4>
                                         {project.language}
@@ -57,18 +109,21 @@ export default function Projects() {
                     }
                 </div>
                 <div className={style.switch}>
-                    <button type="button"> &lt; </button>
+                    <button type="button" onClick={() => moveSide("left")}> &lt; </button>
                     {
                         projects.map((_, i) => {
                             return (
-                                <span key={i} className={i == activeID ? style.hl_bg : ''} />
+                                <span
+                                    key={i}
+                                    onClick={() => scrollTo(i)}
+                                    className={i == activeID ? style.hl_bg : ''}
+                                />
                             )
                         })
                     }
-                    <button type="button"> &gt; </button>
+                    <button type="button" onClick={() => moveSide("right")}> &gt; </button>
                 </div>
             </div>
         </section>
     )
 }
-
